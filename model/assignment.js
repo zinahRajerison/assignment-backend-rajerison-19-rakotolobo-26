cf578cd61bbe4432157ecc3f77e07d9bc4a8827e
zinahRajerison <zinahrajerison2000@gmail.com>
2023-05-17 11:54:27 +0200

    first commit

---
+++ model/assignment.js
@@ -0,0 +1,19 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+var aggregatePaginate = require("mongoose-aggregate-paginate-v2");
+
+let AssignmentSchema = Schema({
+    id: Number,
+    dateDeRendu: Date,
+    nom: String,
+    rendu: Boolean
+});
+
+AssignmentSchema.plugin(aggregatePaginate);
+
+// C'est à travers ce modèle Mongoose qu'on pourra faire le CRUD
+// le nom de la collection (par défaut assignments) sera au pluriel, 
+// soit assignments
+// Si on met un nom "proche", Mongoose choisira la collection
+// dont le nom est le plus proche
+module.exports = mongoose.model('assignments', AssignmentSchema);

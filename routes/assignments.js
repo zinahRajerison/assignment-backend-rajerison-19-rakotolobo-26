698c541750423ba6308d2b21a480ed736f8e8257
zinahRajerison <zinahrajerison2000@gmail.com>
2023-06-18 21:47:54 +0200

    Update assignments.js

--- routes/assignments.js
+++ routes/assignments.js
@@ -2,92 +2,127 @@ let Assignment = require('../model/assignment');
 
 // Récupérer tous les assignments (GET)
 function getAssignmentsSansPagination(req, res){
-    Assignment.find((err, assignments) => {
-        if(err){
-            res.send(err)
-        }
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        Assignment.find((err, assignments) => {
+            if(err){
+                res.send(err)
+            }
 
-        res.send(assignments);
-    });
+            res.send(assignments);
+        });
+    })
 }
 
 function getAssignments(req, res) {
-    var aggregateQuery = Assignment.aggregate();
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
     
-    Assignment.aggregatePaginate(aggregateQuery,
-      {
-        page: parseInt(req.query.page) || 1,
-        limit: parseInt(req.query.limit) || 10,
-      },
-      (err, assignments) => {
-        if (err) {
-          res.send(err);
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        var aggregateQuery = Assignment.aggregate();
+        
+        Assignment.aggregatePaginate(aggregateQuery,
+        {
+            page: parseInt(req.query.page) || 1,
+            limit: parseInt(req.query.limit) || 10,
+        },
+        (err, assignments) => {
+            if (err) {
+            res.send(err);
+            }
+            res.send(assignments);
         }
-        res.send(assignments);
-      }
-    );
+        );
+    })
    }
    
 // Récupérer un assignment par son id (GET)
 function getAssignment(req, res){
-    let assignmentId = req.params.id;
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        let assignmentId = req.params.id;
 
-    Assignment.findOne({_id: assignmentId}, (err, assignment) =>{
-        if(err){res.send(err)}
-        res.json(assignment);
+        Assignment.findOne({_id: assignmentId}, (err, assignment) =>{
+            if(err){res.send(err)}
+            res.json(assignment);
+        })
     })
 }
 
 // Ajout d'un assignment (POST)
 function postAssignment(req, res){
-    let assignment = new Assignment();
-    // assignment.id = req.body.id;
-    assignment.nom = req.body.nom;
-    assignment.dateDeRendu = req.body.dateDeRendu;
-    assignment.sujet = req.body.sujet;
-    assignment.rendu = req.body.rendu;
-    assignment.id_auteur = req.body.id_auteur;
-    assignment.id_matiere = req.body.id_matiere;
-    assignment.note = req.body.note;
-    assignment.remarques = req.body.remarques;
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        let assignment = new Assignment();
+        // assignment.id = req.body.id;
+        assignment.nom = req.body.nom;
+        assignment.dateDeRendu = req.body.dateDeRendu;
+        assignment.sujet = req.body.sujet;
+        assignment.rendu = req.body.rendu;
+        assignment.id_auteur = req.body.id_auteur;
+        assignment.id_matiere = req.body.id_matiere;
+        assignment.note = req.body.note;
+        assignment.remarques = req.body.remarques;
 
-    console.log("POST assignment reçu :");
-    console.log(assignment)
+        console.log("POST assignment reçu :");
+        console.log(assignment)
 
-    assignment.save( (err) => {
-        if(err){
-            res.send('cant post assignment ', err);
-        }
-        res.json({ message: `${assignment.nom} saved!`})
+        assignment.save( (err) => {
+            if(err){
+                res.send('cant post assignment ', err);
+            }
+            res.json({ message: `${assignment.nom} saved!`})
+        })
     })
 }
 
 // Update d'un assignment (PUT)
 function updateAssignment(req, res) {
-    console.log("UPDATE recu assignment : ");
-    console.log(req.body);
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
     
-    Assignment.findByIdAndUpdate(req.body._id, req.body, {new: true}, (err, assignment) => {
-        if (err) {
-            console.log(err);
-            res.send(err)
-        } else {
-          res.json({message: assignment.nom + 'updated'})
-        }
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        console.log("UPDATE recu assignment : ");
+        console.log(req.body);
+        
+        Assignment.findByIdAndUpdate(req.body._id, req.body, {new: true}, (err, assignment) => {
+            if (err) {
+                console.log(err);
+                res.send(err)
+            } else {
+            res.json({message: assignment.nom + 'updated'})
+            }
 
-      // console.log('updated ', assignment)
-    });
+        // console.log('updated ', assignment)
+        });
+    })
 
 }
 
 // suppression d'un assignment (DELETE)
 function deleteAssignment(req, res) {
-
-    Assignment.findByIdAndRemove(req.params.id, (err, assignment) => {
-        if (err) {
-            res.send(err);
-        }
-        res.json({message: `${assignment.nom} deleted`});
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        Assignment.findByIdAndRemove(req.params._id, (err, assignment) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json({message: `${assignment.nom} deleted`});
+        })
     })
 }
 

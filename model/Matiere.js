9b6dd0745cf74586222c9c87d21c6b250fb47b33
TahianaRakotolobo <tahianarakotolobo@gmail.com>
2023-06-20 22:36:10 +0200

    Matiere objectId

--- model/Matiere.js
+++ model/Matiere.js
@@ -1,8 +1,12 @@
 var mongoose = require('mongoose');  
+let Schema = mongoose.Schema;
 var MatiereSchema = new mongoose.Schema({ 
     nom: String,
     image: String, 
-    id_user:String
+    id_user:{
+        type: Schema.Types.ObjectId,
+        ref: "users",
+    }
 });
 mongoose.model('Matiere', MatiereSchema);
 
